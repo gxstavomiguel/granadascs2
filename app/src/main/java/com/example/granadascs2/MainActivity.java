a923265cb4c9692e5afba998b396b76369065644
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-05-26 21:32:04 -0300

    Modificações em botões

--- app/src/main/java/com/example/granadascs2/MainActivity.java
+++ app/src/main/java/com/example/granadascs2/MainActivity.java
@@ -4,11 +4,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.VideoView;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -58,14 +61,29 @@ public class MainActivity extends AppCompatActivity {
         VideoView videoView = new VideoView(this);
         Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + videoResId);
         videoView.setVideoURI(videoUri);
-        videoView.setOnPreparedListener(mp -> mp.setLooping(true));  // Loop the video
-        videoView.start();
+
+        Button playPauseButton = new Button(this);
+        playPauseButton.setText("Play");
+        playPauseButton.setBackgroundColor(getResources().getColor(R.color.purple));
+        playPauseButton.setOnClickListener(v -> {
+            if (videoView.isPlaying()) {
+                videoView.pause();
+                playPauseButton.setText("Play");
+            } else {
+                videoView.start();
+                playPauseButton.setText("Pause");
+            }
+        });
 
         TextView textView = new TextView(this);
         textView.setText(description);
-        textView.setTextColor(getResources().getColor(android.R.color.white));
+        textView.setTextColor(getResources().getColor(android.R.color.black));
 
         videoContainer.addView(textView);
         videoContainer.addView(videoView);
+        videoContainer.addView(playPauseButton);
+
+        // Start video preparation after adding play/pause button
+        videoView.setOnPreparedListener(mp -> mp.setLooping(true));  // Loop the video
     }
 }
